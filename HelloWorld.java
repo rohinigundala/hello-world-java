9ae948a287995bb7c83f8e05e861b6e3ad15c919
rohinigundala <116956721+rohinigundala@users.noreply.github.com>
2022-11-10 12:40:24 +0530

    Update HelloWorld.java
    updated class name to main

--- HelloWorld.java
+++ HelloWorld.java
@@ -32,7 +32,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class main {
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
