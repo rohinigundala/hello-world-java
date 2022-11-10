c4e4b7486eb8d9bae7580d76e02412b654cea899
rohinigundala <116956721+rohinigundala@users.noreply.github.com>
2022-11-10 16:18:24 +0530

    Update HelloWorld.java
    changed back to helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -32,7 +32,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class main {
+public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
